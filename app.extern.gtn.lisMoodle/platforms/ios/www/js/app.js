a7dc619fb6ced59cd3812935930f689919aa68f9
Martin Kattner <martin.kattner@stygs.com>
2014-03-07 09:57:12 -0800

    Compile iOS

--- app.extern.gtn.lisMoodle/platforms/ios/www/js/app.js
+++ app.extern.gtn.lisMoodle/platforms/ios/www/js/app.js
@@ -1,24 +1,38 @@
+/* on jquery mobile initialisation */
 $(document).bind("mobileinit", function() {
 	app.debug("mobileinit", 2);
 	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
 	$.mobile.page.prototype.options.domCache = false;
 	settings.initSettings();
+	app.cordovaAvailable = true;
 });
 
+/* on cordova initialisation */
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 	;// alert("device ready");
 }
 
-$(document).on('pagebeforecreate', function() {
-	app.debug("pagebeforecreate: each page");
+/* on pagebeforecreate */
+$(document).on('pagebeforecreate', function(event) {
+	app.debug("pagebeforecreate: each page", 1);
 });
-
+$(document).on('pageshow', function(event) {
+	app.debug("pageshow: each page");
+	var pageId = $.mobile.activePage.attr('id');
+	if (app.debugDevice && pageId) {
+		app.appendDebugArea(pageId);
+	}
+});
 var app = {
-	doDebuging : false,
-	debugLevel : 1,
+	debugDevice : true,
+	doDebuging : "false",
+	debugLevel : 2,
+	cordovaAvailable : false,
+	init : function() {
+	},
 	debug : function(text, level) {
 		// alert("Level:" + level + "DoDebuging" + this.doDebuging);
 		if (level && this.doDebuging == "true") {
@@ -39,5 +53,44 @@ var app = {
 	},
 	confirm : function(title, text, sucsessPage, unsucsessPage) {
 		;
+	},
+	debugAreaCode : function() {
+		var htmlCode = '';
+		htmlCode += '<div id="divDebugCheckbox" data-role="fieldcontain">';
+		htmlCode += '<fieldset class="debugGroup" data-role="controlgroup">';
+		htmlCode += '<legend>Debug Area:</legend>';
+		htmlCode += '<input type="button" name="btnRefresh" id="btnRefresh" class="custom" value="Refresh Page"/>';
+		htmlCode += '<input type="checkbox" name="cbxToggleDebug" id="cbxToggleDebug" class="custom" />';
+		htmlCode += '<label for="cbxToggleDebug">Toggle Debug</label>';
+		htmlCode += '</fieldset>';
+		htmlCode += '</div>';
+		htmlCode += '';
+		return htmlCode;
+	},
+	appendDebugArea : function(pageId) {
+		if (!($("#" + pageId + " #divDebug").length > 0))
+			$("#" + pageId).append('<div id="divDebug">$nbsp;</div>');
+		$("#" + pageId + " #divDebug").empty();
+		var htmlCode = app.debugAreaCode();
+		$("#" + pageId + " #divDebug").append(htmlCode);
+		app.initDebugAreaEvents(pageId);
+		$("#" + pageId + " #divDebug .debugGroup").trigger('create');
+
+	},
+	initDebugAreaEvents : function(pageId) {
+		// alert(Object.prototype.toString.call(app.doDebuging).replace(/^\[object
+		// (.+)\]$/, "$1").toLowerCase() + " Val: ;" + app.doDebuging + ";");
+		if (app.doDebuging == "true") {
+			$("#" + pageId + " #cbxToggleDebug").prop('checked', true);
+		} else {
+			$("#" + pageId + " #cbxToggleDebug").prop('checked', false);
+		}
+		$("#" + pageId + " #cbxToggleDebug").change(function() {
+			window.localStorage.setItem('data-app-debug', $("#" + pageId + " #cbxToggleDebug").prop('checked'));
+			settings.initSettings();
+		});
+		$("#" + pageId + " #btnRefresh").on("click", function() {
+			location.reload();
+		});
 	}
 };
