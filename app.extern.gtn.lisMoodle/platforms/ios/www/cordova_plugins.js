52082b0ed84e2196035b7d85a9b95ebc0889b3d4
Martin Kattner <martin.kattner@stygs.com>
2014-03-25 17:45:28 -0700

    iOS

--- app.extern.gtn.lisMoodle/platforms/ios/www/cordova_plugins.js
+++ app.extern.gtn.lisMoodle/platforms/ios/www/cordova_plugins.js
@@ -36,6 +36,13 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.file.DirectoryEntry",
         "clobbers": [
@@ -186,6 +193,13 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.media/www/MediaError.js",
         "id": "org.apache.cordova.media.MediaError",
         "clobbers": [
@@ -247,20 +261,6 @@ module.exports = [
         "clobbers": [
             "navigator.device.capture"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
-        "id": "org.apache.cordova.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -268,12 +268,12 @@ module.exports.metadata =
 {
     "org.apache.cordova.camera": "0.2.8",
     "org.apache.cordova.device": "0.2.8",
+    "org.apache.cordova.dialogs": "0.2.6",
     "org.apache.cordova.file": "1.0.1",
     "org.apache.cordova.file-transfer": "0.4.2",
-    "org.apache.cordova.media": "0.2.9",
-    "org.apache.cordova.media-capture": "0.2.8",
     "org.apache.cordova.inappbrowser": "0.3.3",
-    "org.apache.cordova.dialogs": "0.2.6"
+    "org.apache.cordova.media": "0.2.9",
+    "org.apache.cordova.media-capture": "0.2.8"
 }
 // BOTTOM OF METADATA
 });
